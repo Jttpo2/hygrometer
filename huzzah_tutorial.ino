cfdaf5f74893148cfbbfb7ce65aa3d489228ff43
John Petersson <johnpetersson@gmail.com>
2017-06-22 17:31:22 +0200

    Using deep sleep to prolong battery life.

--- huzzah_tutorial.ino
+++ huzzah_tutorial.ino
@@ -14,11 +14,12 @@ AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 #include <Adafruit_MQTT.h>
 #include <ArduinoHttpClient.h>
 
-#include <DHT.h>
+#include <EEPROM.h>
 
-#define ONBOARD_LED_PIN 0
-#define LED_PIN 13
-#define BUTTON_PIN 4
+#define BATTERY_INTERVAL 10e6 //
+#define SLEEP_LENGTH 10e6
+
+#include <DHT.h>
 
 #define HUMIDITY_SENSOR_PIN 12
 #define DHTTYPE DHT22
@@ -27,6 +28,10 @@ DHT dht(HUMIDITY_SENSOR_PIN, DHTTYPE);
 // Humidity variables
 float humidity;
 float temperature; 
+
+#define ONBOARD_LED_PIN 0
+#define LED_PIN 13
+#define BUTTON_PIN 4
 
 // Button state
 int current = LOW;
@@ -39,18 +44,74 @@ AdafruitIO_Feed *humidityCommand = io.feed("humidity");
 AdafruitIO_Feed *temperatureCommand = io.feed("temperature");
 AdafruitIO_Feed *buttonCommand = io.feed("button");
 
+// Sketch restarts after sleep, so loop() never runs
 void setup() {
+  setupSerial();
+  setupPins();
+  setupHumiditySensor();
+//  connectToAdafruitIO();
+//
+  runHumiditySensor();
+  printHumidityReadings();
+//  sendReadingsToCloud();
+
+//  handleBatterySensing();
+//
+//  delay(5*1000);
+  hibernate();
+}
+
+// Sketch restarts after sleep, so loop() never runs
+void loop() {
+  // Required at the top to keep talking to io.adafruit.com
+//  io.run();
+//
+//  runHumiditySensor();
+//  printHumidityReadings();
+//  sendReadingsToCloud();
+//
+//  handleButtonSensing();
+//  sendButtonStatusToCloud();
+
+//  delay(5000);
+}
+
+void handleMessage(AdafruitIO_Data *data) {
+  int command = data->toInt();
+//  if (command == 1) {
+//    Serial.print("received <- ");
+//    Serial.println(command);
+//    blinkLED();
+//  } else {
+    Serial.print("received <- ");
+    Serial.println(command);
+
+//    Serial.println(data);
+//  }
+}
+
+void setupPins() {
+  Serial.println("Setting up pins");
   pinMode(ONBOARD_LED_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(HUMIDITY_SENSOR_PIN, INPUT);
+}
 
+void setupSerial() {
   Serial.begin(115200);
+  while (!Serial) { }
   delay(50);
+  Serial.println("Serial connection initiated");
+}
 
+void setupHumiditySensor() {
+  Serial.println("Initializing DHT sensor");
   dht.begin();
+}
 
-  Serial.print("Connecting to Adafruit IO");
+void connectToAdafruitIO() {
+  Serial.print("Connecting to Adafruit IO");  
   io.connect();
 
   // Message handler receives messages from adafruit.io
@@ -69,34 +130,8 @@ void setup() {
   Serial.println(io.statusText());
   Serial.print("Local IP: ");
   Serial.println(WiFi.localIP());
-}
 
-void loop() {
-  // Required at the top to keep talking to io.adafruit.com
   io.run();
-
-  runHumiditySensor();
-  printHumidityReadings();
-  sendReadingsToCloud();
-
-  handleButtonSensing();
-  sendButtonStatusToCloud();
-
-  delay(5000);
-}
-
-void handleMessage(AdafruitIO_Data *data) {
-  int command = data->toInt();
-//  if (command == 1) {
-//    Serial.print("received <- ");
-//    Serial.println(command);
-//    blinkLED();
-//  } else {
-    Serial.print("received <- ");
-    Serial.println(command);
-
-//    Serial.println(data);
-//  }
 }
 
 void blinkPin(int pin) {
@@ -128,6 +163,10 @@ void printHumidityReadings() {
 }
 
 void sendReadingsToCloud() {
+  
+//  humidityCommand->save(80);
+//  temperatureCommand->save(100); 
+  
   Serial.print("Sending humidity -> ");
   Serial.println(humidity);
   humidityCommand->save(humidity);
@@ -137,11 +176,45 @@ void sendReadingsToCloud() {
   temperatureCommand->save(temperature); 
 }
 
+void handleBatterySensing() {
+   EEPROM.begin(512);
+
+  // Get current count position from eeprom
+  byte battery_count = EEPROM.read(0);
+
+  // Use eeprom to track battery count between resets
+  if (battery_count >= (BATTERY_INTERVAL / SLEEP_LENGTH)) {
+    // Reset counter
+    battery_count = 0;
+    runBatterySensor();
+    sendBatteryReadingToCloud();
+  } else {
+    battery_count++;
+  }
+
+  // Save current count
+  EEPROM.write(0, battery_count);
+  EEPROM.commit();
+}
+
+void runBatterySensor() {
+  Serial.println("Reading battery level");
+}
+
+void sendBatteryReadingToCloud() {
+  Serial.println("Sending battery level to cloud");
+}
+
 void sendButtonStatusToCloud() {
    // Save button state to command feed on adafruit io
   Serial.print("Sending button -> ");
   Serial.println(current);
   buttonCommand->save(current);
+}
+
+void hibernate() {
+  Serial.println("Going to sleep");
+  ESP.deepSleep(SLEEP_LENGTH, WAKE_RF_DISABLED); 
 }
 
 void handleButtonSensing() {
@@ -160,6 +233,5 @@ void handleButtonSensing() {
   digitalWrite(LED_PIN, current);
 
   last = current;
-
 }
 
